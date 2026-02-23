04a3568c317f33ddc62e83c7bfa90c4fb9fa9675
mdrayhan03 <phylosopherhossain2022@gmail.com>
2026-02-23 20:55:58 +0600

    static file updated

--- ridebuddy/ridebuddy/settings.py
+++ ridebuddy/ridebuddy/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",   # Your project-level static folder
+    BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
